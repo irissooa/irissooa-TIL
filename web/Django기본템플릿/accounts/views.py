3abc52e4890ba041e0baf93ad925d417b4ed943d
IRIS <xlalfskdl@gmail.com>
2020-10-04 23:24:35 +0900

    1004

--- web/Django기본템플릿/accounts/views.py
+++ web/Django기본템플릿/accounts/views.py
@@ -35,7 +35,7 @@ def login(request):
         form = AuthenticationForm(request,request.POST)
         if form.is_valid():
             auth_login(request,form.get_user())
-            return redirect(request.GET.get('next') or 'articles:index')
+            return redirect('articles:index')
 
     else:
         form = AuthenticationForm()
